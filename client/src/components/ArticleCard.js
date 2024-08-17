9f251f2c152cc31e15dd809d57943c2032988447
Atsushi <atu729@icloud.com>
2024-08-18 01:26:31 +0900

    add description property

--- client/src/components/ArticleCard.js
+++ client/src/components/ArticleCard.js
@@ -1,17 +1,24 @@
-import React from 'react';
-import './ArticleCard.css';
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import "./ArticleCard.css";
 
-const ArticleCard = ({ image, title, description, date }) => {
-    return (
-        <div className="article-card">
-            <img src={image} alt={title} className="article-image" />
-            <div className="article-content">
-                <a href='/article-page'><h2 className="article-title">{title}</h2></a>
-                <p className="article-description">{description}</p>
-                <p className="article-date">{date}</p>
-            </div>
-        </div>
-    );
+const ArticleCard = ({ id, image, title, description, date }) => {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate(`/article/${id}`);
+  };
+
+  return (
+    <div className="article-card" onClick={handleClick}>
+      <img src={image} alt={title} className="article-image" />
+      <div className="article-content">
+        <h2 className="article-title">{title}</h2>
+        <p className="article-description">{description}</p>
+        <p className="article-date">{date}</p>
+      </div>
+    </div>
+  );
 };
 
 export default ArticleCard;
--- server/models/article.js
+++ server/models/article.js
@@ -1,27 +1,31 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const articleSchema = new mongoose.Schema({
-    title: {
-        type: String,
-        required: true,
-    },
-    date: {
-        type: String,
-        required: true,
-    },
-    author: {
-        type: String,
-        required: true,
-    },
-    content: {
-        type: String,
-        required: true,
-    },
-    image: {
-        type: String,
-        required: true,
-    },
+  title: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: String,
+    required: true,
+  },
+  author: {
+    type: String,
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
 });
 
-const Article = mongoose.model('Article', articleSchema);
+const Article = mongoose.model("Article", articleSchema);
 module.exports = Article;
