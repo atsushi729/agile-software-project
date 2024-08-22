5d34a56b40823001d5ef216a13058cf4791ccb15
Atsushi <atu729@icloud.com>
2024-08-22 20:08:49 +0900

    add response value

--- client/src/pages/GenerateRecipePage.js
+++ client/src/pages/GenerateRecipePage.js
@@ -1,22 +1,31 @@
-import React from "react";
+import React, { useRef } from "react";
 import "./GenerateRecipePage.css";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import "./RecipePage.css";
 
 const GenerateRecipePage = () => {
   const [recipe, setRecipe] = React.useState(null);
   const [loading, setLoading] = React.useState(false);
 
+  const loadingRef = useRef(null);
+
   const handleSubmit = (event) => {
     event.preventDefault();
     setLoading(true);
+    setRecipe(null);
+
+    setTimeout(() => {
+      if (loadingRef.current) {
+        loadingRef.current.scrollIntoView({ behavior: "smooth" });
+      }
+    }, 0);
+
     const ingredients = event.target.elements.ingredients.value
       .split(",")
       .map((ingredient) => ingredient.trim());
     const API_ENDPOINT = process.env.API_ENDPOINT || "http://localhost:3000";
-    console.log(ingredients);
 
-    // Call the API to generate a recipe
     fetch(`${API_ENDPOINT}/generate-recipe`, {
       method: "POST",
       headers: {
@@ -32,7 +41,7 @@ const GenerateRecipePage = () => {
       })
       .then((data) => {
         console.log(data);
-        setRecipe(data.recipe); // Assuming the response contains a JSON object for the recipe
+        setRecipe(data.recipe);
         setLoading(false);
       })
       .catch((error) => {
@@ -61,16 +70,68 @@ const GenerateRecipePage = () => {
         </section>
       </main>
       {loading && (
-        <div className="loading-container">
+        <div className="loading-container" ref={loadingRef}>
           <div className="spinner"></div>
-          <p>Loading...</p>
+          <p className="loading-text">Generating...</p>
         </div>
       )}
       {recipe && (
-        <div className="recipe-card">
+        <div className="recipe-container">
           <h2>{recipe.title}</h2>
           <p>{recipe.description}</p>
           <img src={recipe.image} alt={recipe.title} />
+          <section className="recipe-ingredients">
+            <h2>Ingredients</h2>
+            <ul>
+              {recipe.ingredients.map((ingredient, index) => (
+                <li key={index}>
+                  <span>{index + 1}</span> {ingredient}{" "}
+                  <input type="checkbox" />
+                </li>
+              ))}
+            </ul>
+          </section>
+          <section className="recipe-directions">
+            <h2>Directions</h2>
+            <ul>
+              {Object.entries(recipe.instructions).map(([key, value]) => (
+                <li key={key}>
+                  <span>{key}</span> {value} <input type="checkbox" />
+                </li>
+              ))}
+            </ul>
+          </section>
+          <section className="nutrition-info">
+            <h2>Nutrition Information</h2>
+            <table>
+              <tbody>
+                <tr>
+                  <td>Calories</td>
+                  <td>{recipe.nutrition.calories}</td>
+                </tr>
+                <tr>
+                  <td>Total Fat</td>
+                  <td>{recipe.nutrition.totalFat}</td>
+                </tr>
+                <tr>
+                  <td>Saturated Fat</td>
+                  <td>{recipe.nutrition.saturatedFat}</td>
+                </tr>
+                <tr>
+                  <td>Cholesterol</td>
+                  <td>{recipe.nutrition.cholesterol}</td>
+                </tr>
+                <tr>
+                  <td>Sodium</td>
+                  <td>{recipe.nutrition.sodium}</td>
+                </tr>
+                <tr>
+                  <td>Total Carbohydrates</td>
+                  <td>{recipe.nutrition.totalCarbohydrates}</td>
+                </tr>
+              </tbody>
+            </table>
+          </section>
         </div>
       )}
       <Footer />
