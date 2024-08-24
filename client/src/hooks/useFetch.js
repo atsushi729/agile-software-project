32933b765c286be15b9e65571693425812b7b965
Atsushi <atu729@icloud.com>
2024-08-24 14:28:37 +0900

    add custom hooks to fetch API

---
+++ client/src/hooks/useFetch.js
@@ -0,0 +1,36 @@
+import { useState, useEffect } from "react";
+
+const useFetch = (url) => {
+  const [data, setData] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  // prettier-ignore
+  useEffect(() => {
+    const BASE_API_ENDPOINT = process.env.API_ENDPOINT || "http://localhost:3000";
+
+    console.log(`Fetching data from ${BASE_API_ENDPOINT}${url}`);
+
+    fetch(`${BASE_API_ENDPOINT}${url}`)
+      .then((response) => {
+        if (!response.ok) {
+          // error coming back from server
+          throw Error("could not fetch the data for that resource");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setIsLoading(false);
+        setData(data);
+        setError(null);
+      })
+      .catch((err) => {
+        setIsLoading(false);
+        setError(err.message);
+      });
+  }, [url]);
+
+  return { data, isLoading, error };
+};
+
+export default useFetch;
